4060fb709b89855a584e39e0003ab1a958640cc7
Tumucin <tumukhan@icloud.com>
2025-09-17 20:46:49 +0200

    Betu a termekekben

---
+++ src/routes/_card.svelte
@@ -0,0 +1,51 @@
+<script>
+    let { product } = $props()
+</script>
+
+<div class="product-card">
+    <div class="img" style="background-image: url({product.image});"></div>
+    <h2>{product.title}</h2>
+    <span class="price">{product.price}</span>
+</div>
+
+<style>
+    .product-card {
+        padding: 5px;
+        cursor: pointer;
+        z-index: 20;
+        gap: 0px;
+    }
+
+    @media screen and (max-width: 780px) {
+    .product-card {
+        padding: 0px;
+        grid-template-columns: repeat(2, 1fr);
+        gap: 5px;
+    }
+}
+
+    .product-card:hover {
+        z-index: 21
+    }
+
+    h2 {
+        position: relative;
+        font-family: 'Orbitron', sans-serif;
+        font-size: 1rem;
+        left: 0.5rem;
+    }
+
+    .price {
+        position: relative;
+        left: 0.5rem;
+        font-size: 0.9rem;
+        font-weight: bold;
+    }
+    
+    .img {
+        width: 100%;
+        height: 300px;
+        background-position: center;
+        background-size: cover;
+    }
+</style>
--- src/routes/_container.svelte
+++ src/routes/_container.svelte
@@ -1,20 +1,18 @@
 <script>
+    import Card from './_card.svelte'
+
     export let products = [
         { image: "https://images.squarespace-cdn.com/content/v1/6204821bfe06b76898b431c5/1be69659-50e9-403d-b5e5-2804d131ad3e/IMG_4377.JPG", title: "Termék 1", description: "Leírás 1", price: "$9.99" },
         { image: "https://images.unsplash.com/photo-1709004915865-38bc70f4cb78?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bW9kZWwlMjBtYW58ZW58MHx8MHx8fDA%3D", title: "Termék 2", description: "Leírás 2", price: "$12.99" },
         { image: "https://c4.wallpaperflare.com/wallpaper/523/513/195/snsd-yoona-im-yoona-korean-women-model-asian-hd-wallpaper-preview.jpg", title: "Termék 3", description: "Leírás 3", price: "$15.99" },
-        { image: "https://w0.peakpx.com/wallpaper/334/1020/HD-wallpaper-jennie-blackpink-korean-singer-model-thumbnail.jpg", title: "Termék 4", description: "Leírás 4", price: "$19.99" }
+        { image: "https://w0.peakpx.com/wallpaper/334/1020/HD-wallpaper-jennie-blackpink-korean-singer-model-thumbnail.jpg", title: "Termék 4", description: "Leírás 4", price: "$19.99" },
+        { image: "https://cdn.tatlerasia.com/tatlerasia/i/2023/03/28185238-bts-jungkook-calvin-klein-campaign-2023-underwear-model-kpop-ambassador-sp23-jung-kook-3-photo-credit-park-jong-ha-cropped_cover_1599x837.jpg", title: "Jungkook", description: "Jungkook", price: "$1 929 900,99"  }
     ];
 </script>
 
 <div class="product-grid">
     {#each products as product}
-        <div class="product-card">
-            <img src={product.image} alt={product.title}>
-            <h2>{product.title}</h2>
-            <p>{product.description}</p>
-            <span class="price">{product.price}</span>
-        </div>
+        <Card {product} />
     {/each}
 </div>
 
@@ -22,7 +20,6 @@
 .product-grid {
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
-    gap: 15px;
     padding: 15px; 
 }
 
@@ -30,54 +27,8 @@
 @media screen and (max-width: 780px) {
     .product-grid {
         grid-template-columns: repeat(2, 1fr);
-        gap: 2px;
-        padding: 0px;
+        gap: 5px;
     }
 }
-
-/* Product card */
-.product-card {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 1px;
-    background: rgba(255,255,255,0.1);
-    backdrop-filter: blur(10px) saturate(180%);
-    text-align: center;
-    color: white;
-    transition: transform 0.2s ease, box-shadow 0.2s ease;
-    backdrop-filter: blur(8px);
-    padding-bottom: 10px;
-    z-index: 10;
-}
-
-.product-card:hover {
-    z-index: 11;
-}
-
-.product-card img {
-    width: 100%;
-    height: 300px;
-    object-fit: cover;
-}
-
-h2 {
-    font-family: 'Orbitron', sans-serif;
-    font-size: 1rem;
-    color: #fff;
-    text-shadow: 0 0 5px #a777e3, 0 0 10px #a777e3, 0 0 20px #6e8efb;
-}
-
-p {
-    font-size: 0.8rem;
-    color: rgba(255,255,255,0.8);
-}
-
-.price {
-    font-size: 0.9rem;
-    font-weight: bold;
-    color: #a777e3;
-}
-
 
 </style>
